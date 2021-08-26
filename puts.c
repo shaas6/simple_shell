3f088dbb6a6e7f74a05bb392f87319b9f6e66bf5
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-08-26 18:18:07 -0400

    adding helper functions

---
+++ puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts - writes an array of chars. No newline
+ * @str: pointer to string
+ *
+ */
+void _puts(char *str)
+{
+    while (*str != 0) /* loop through char array by pointer */
+    {
+        _putchar(*str);
+        str++;
+    }
+}
