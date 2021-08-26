ef68668259f03e6cb8c5087f8b8652a6b571a871
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-08-26 18:12:09 -0400

    prototypes

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+
+#endif
