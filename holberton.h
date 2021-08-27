437e49ebea2aef2320d4023456c4b3760f160065
shaas6 <seanhaas@live.com>
2021-08-27 10:39:54 -0400

    Adding more prototypes to the header

--- holberton.h
+++ holberton.h
@@ -5,9 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
+int main(int ac, char **av);
 int _putchar(char c);
 void _puts(char *str);
-int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+void new_prompt(void);
 
 #endif
