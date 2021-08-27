2692f16d68c129c702badf67fc8af06f6758d831
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-08-27 11:02:09 -0400

    added code

--- shell.c
+++ shell.c
@@ -1,12 +1,22 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * the simplest of shells 
  */
 
 int main(int ac, char **av)
 {
+    char *input;
+    size_t size;
+    ssize_t status;
 
+    while (1)
+    {
+        status = getline(&input, &size, stdin);
+        if (status == -1)
+         {  
+            perror("Input error")
+         }
+    }
+    return (0);
 }
