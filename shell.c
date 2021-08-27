78b6ef40383fb42d2b3f642727cbea44a4bb7c6d
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-08-27 13:28:06 -0400

    codes broke

--- shell.c
+++ shell.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * the simplest of shells
  * erroneous comment 
@@ -7,16 +8,25 @@
 
 int main(int ac __attribute__((unused)), char **av)
 {
+    char buf[900];
     char *input;
     size_t size;
-    
+    ssize_t stat;
+
     while (1)
     {
-        **av = getline(&input, &size, stdin);
-        if (**av == -1)
-         {  
-            perror("Input error");
-         }
+        stat = getline(&input, &size, stdin);
+        if (strlen(input==0))
+        {
+                    strcpy(buf,"\n");
+        }
+        else     
+        {
+            strcpy(buf, input);
+        }   
+        if(strcomp(input,"exit")==0)
+        {
+            exit(0);
+        }
     }
-    return (0);
 }
