d91ad3beb2489ead6f0ce2637f154a1ce8e8d45a
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-08-27 11:15:19 -0400

    added semi colon

--- shell.c
+++ shell.c
@@ -10,13 +10,13 @@ int main(int ac, char **av)
     char *input;
     size_t size;
     ssize_t status;
-
+    
     while (1)
     {
         status = getline(&input, &size, stdin);
         if (status == -1)
          {  
-            perror("Input error")
+            perror("Input error");
          }
     }
     return (0);
