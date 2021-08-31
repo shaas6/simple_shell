ebdc01d8b572c784eff84f8c6737eb62dbdf6858
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-08-31 15:34:59 -0400

    betty fixes

--- printenv.c
+++ printenv.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 /**
  * print_env - prints all environment variables
+ * @env: enviroment variables
  */
 void print_env(char **env)
 {
 	int i;
-	for(i = 0; env[i]; i++)
+
+	for (i = 0; env[i]; i++)
 		puts(env[i]);
 }
