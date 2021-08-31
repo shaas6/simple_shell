8c185aed1c9398a1c505bc36cc15712e03fb7fb1
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-08-31 15:35:13 -0400

    betty fixes

--- shell.c
+++ shell.c
@@ -2,34 +2,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * the simplest of shells 
- */
+* main - the simplest of shells
+* @ac: argument counter
+* @av: argument variable
+* @env: environment variable
+* Return: Nothing
+*/
 int main(int ac __attribute__((unused)), char **av, char **env)
 {
-    
-    char *input = NULL;
-    size_t size;
-    ssize_t stat;
-    
+char *input = NULL;
+size_t size;
+ssize_t stat;
 
-    (void) av;
 
-    if (isatty(STDIN_FILENO)== 1)
-            
-    
-    while (1)
-    {
-        new_prompt();
-        stat = getline(&input, &size, stdin);
-         if (stat == -1)
-                perror("Error");
+(void) av;
 
-        if(_strcmp(input,"exit\n")==0)
-        {
-            exit(0);
-        }
-        else if(_strcmp(input,"env\n")==0)
-            print_env(env);
-    }
+if (isatty(STDIN_FILENO) == 1)
+
+while (1)
+{
+new_prompt();
+stat = getline(&input, &size, stdin);
+if (stat == -1)
+perror("Error");
+
+if (_strcmp(input, "exit\n") == 0)
+{
+exit(0);
 }
-  
+else if (_strcmp(input, "env\n") == 0)
+print_env(env);
+}
+}
