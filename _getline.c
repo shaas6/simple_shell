409981200fe6c082a9419f43a361c69dffdc1b44
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-03 17:06:53 -0400

    new function

---
+++ _getline.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _getline - prints "shell$" and waits for input
+ * Return: line of string input for user
+ */
+
+char *_getline(void)
+{
+    char *linepointer = NULL;
+    size_t user = 0;
+
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "shell$ ", 14);
+
+    if (getline(&linepointer, &user, stdin) == -1)
+    {
+        free(linepointer);
+        return (NULL);
+    }
+    return (linepointer)
+}
