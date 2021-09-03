1c0247cc863300d6c6c856a98afe56bd26d241fe
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-03 16:49:14 -0400

    added library

--- holberton.h
+++ holberton.h
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 /* holds environment variables */
 extern char **environ;
