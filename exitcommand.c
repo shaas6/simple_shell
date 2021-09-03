93ca14df4440a4b0344f07da0ba6a8ba0ee18336
shaas6 <seanhaas@live.com>
2021-09-03 16:19:16 -0400

    Adding exit command file

---
+++ exitcommand.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _exitshell - exits the shell
+ * @ptr: input
+ * @what: arguement pointer
+ * @end: exit value
+ * Return: nothing
+ */
+
+void _exitshell(char **what, char *ptr, int end)
+{
+    int status = 0;
+
+    if (!what[1])
+    {
+        free(ptr);
+        free(what);
+        exit(end);
+    }
+    status = atoi(what[1]);
+
+    free(ptr);
+    free(what);
+    exit(end);
+}
