2f04e3b5540c83adefc237ec8416b79fe6d6766a
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-03 16:46:47 -0400

    parse function

---
+++ get_token.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * 
+ * 
+ * 
+ */
+
+char **get_token(char *linepointer)
+{
+    char **command = NULL;
+    char *token = NULL; 
+    size_t i = 0;
+    int size = 0;
+
+    if (linepointer == NULL)
+            return (NULL);
+    for (i = 0; linepointer[i]; i++)
+    {
+    if (linepointer[i] == ' ')
+            size++;
+    }
+    if ((size + 1) == _strlen(linepointer))
+                return(NULL);
+    command = malloc(sizeof(char *) * (size + 2));
+    if (command == NULL)
+        return (NULL);      
+token = strtok(linepointer, " \n\t\r");
+for (i = 0; token != NULL; i++)
+{
+    command[i] = token;
+    token = strtok(NULL, " \n\t\r");
+}
+command[i] = NULL;
+                return (command);
+}
