61a8a8324bfecbd311ff91f066b98885a7b3cd9e
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-03 17:54:30 -0400

    changed code

---
+++ getenv.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_env - prints all environment variables
+ * @env: enviroment variables
+ */
+void print_env(char **env)
+{
+size_t count = 0;
+
+	while (env[count])
+	{
+		write(STDOUT_FILENO, env[count], _strlen(env[count]));
+		write(STDOUT_FILENO, "\n", 1);
+		run++;
+	}
+}
