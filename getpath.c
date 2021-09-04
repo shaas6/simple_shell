dce98ff9b8046a432393c8c5713f61b645b3bb39
shaas6 <seanhaas@live.com>
2021-09-03 20:33:26 -0400

    Betty fix

--- getpath.c
+++ getpath.c
@@ -7,24 +7,24 @@
 
 char *getpath(char **env)
 {
-    size_t index = 0, variable = 0, count = 5;
-    char *path = NULL;
+	size_t index = 0, variable = 0, count = 5;
+	char *path = NULL;
 
-    for (index = 0; _strncmp(env[index], "PATH=", 5); index++)
-        ;
-    if (env[index] == NULL)
-                return (NULL);
-      
-    for (count = 5; env[index][variable]; variable++, count++)
-        ;
-    path = malloc(sizeof(char) * (count + 1));
+	for (index = 0; _strncmp(env[index], "PATH=", 5); index++)
+		;
+	if (env[index] == NULL)
+		return (NULL);
 
-    if (path == NULL)
-            return (NULL);
+	for (count = 5; env[index][variable]; variable++, count++)
+		;
+	path = malloc(sizeof(char) * (count + 1));
 
-    for (variable = 5, count = 0; env[index][variable]; variable++, count++)
-                path[count] = env[index][variable];
+	if (path == NULL)
+		return (NULL);
 
-    path[count] = '\0';
-    return (path);
+	for (variable = 5, count = 0; env[index][variable]; variable++, count++)
+		path[count] = env[index][variable];
+
+	path[count] = '\0';
+	return (path);
 }
