453641976d9c78bec70bf5b8bb6baefcf974f95a
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-03 20:09:58 -0400

    fixed

--- fork_func.c
+++ fork_func.c
@@ -4,39 +4,39 @@
  * @arg: command and values path
  * @av: name of program
  * @env: environment
- * @linepointer: command line for the user
+ * @lineptr: command line for the user
  * @id: id of process
  * @c: checker add new test
+ * Return: 0 if successful
  */
-
-int fork_func(char **arg, char **av, char **env, char *linepointer, int id, int c)
+int fork_func(char **arg, char **av, char **env, char *lineptr, int id, int c)
 {
-    pid_t childPID;
-    int status;
-    char *format = "%s: %d: %s: not found\n";
+	pid_t childPID;
+	int status;
+	char *format = "%s: %d: %s: not found\n";
 
-    childPID = fork();
+	childPID = fork();
 
-    if (arg == NULL)
-            exit(errno);
+	if (arg == NULL)
+		exit(errno);
 
-    if (childPID == 0)
-    {
-        if (execve(arg[0], arg, env) == -1)
-        {
-            fprintf(stderr, format, av[0], id, arg[0]);
-            if (!c)
-                    free(arg[0]);
-                free(arg);
-                free(linepointer);
-                exit(errno);
-        }
-    }
-    else
-    {
-            wait(&status);
-            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-                    return (WEXITSTATUS(status));
-    }
-    return (0);
+	if (childPID == 0)
+	{
+		if (execve(arg[0], arg, env) == -1)
+		{
+			fprintf(stderr, format, av[0], id, arg[0]);
+			if (!c)
+				free(arg[0]);
+			free(arg);
+			free(linepointer);
+			exit(errno);
+		}
+	}
+	else
+		{
+			wait(&status);
+			if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			return (WEXITSTATUS(status));
+		}
+		return (0);
 }
--- get_token.c
+++ get_token.c
@@ -1,36 +1,34 @@
 #include "holberton.h"
-
 /**
  * get_token - get token of string
  * @linepointer: command user
  * Return: to a pointer
  */
-
 char **get_token(char *linepointer)
 {
-    char **command = NULL;
-    char *token = NULL; 
-    size_t i = 0;
-    int size = 0;
+	char **command = NULL;
+	char *token = NULL;
+	size_t i = 0;
+	int size = 0;
 
-    if (linepointer == NULL)
-            return (NULL);
-    for (i = 0; linepointer[i]; i++)
-    {
-    if (linepointer[i] == ' ')
-            size++;
-    }
-    if ((size + 1) == _strlen(linepointer))
-                return(NULL);
-    command = malloc(sizeof(char *) * (size + 2));
-    if (command == NULL)
-        return (NULL);      
-token = strtok(linepointer, " \n\t\r");
-for (i = 0; token != NULL; i++)
-{
-    command[i] = token;
-    token = strtok(NULL, " \n\t\r");
-}
-command[i] = NULL;
-                return (command);
+	if (linepointer == NULL)
+		return (NULL);
+	for (i = 0; linepointer[i]; i++)
+	{
+		if (linepointer[i] == ' ')
+		size++;
+	}
+	if ((size + 1) == _strlen(linepointer))
+		return (NULL);
+		command = malloc(sizeof(char *) * (size + 2));
+	if (command == NULL)
+		return (NULL);
+	token = strtok(linepointer, " \n\t\r");
+		for (i = 0; token != NULL; i++)
+		{
+			command[i] = token;
+			token = strtok(NULL, " \n\t\r");
+		}
+		command[i] = NULL;
+		return (command);
 }
